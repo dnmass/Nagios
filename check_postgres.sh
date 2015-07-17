211261595d194a4d01c683c13313bad0ce213108
David Massie <david.n.massie@gmail.com>
2015-07-17 09:56:31 -0500

    Create check_postgres.sh

---
+++ check_postgres.sh
@@ -0,0 +1,15 @@
+HOST=`dockers02`
+PORT=`49230`
+
+/usr/bin/check_postgres --action=connection --host=$HOST --port=$PORT  --dbuser=admin --dbpass=admin --output=simple  | while read rc
+do
+if [ $rc -eq 1 ]
+then echo "postgres check on $HOST:$PORT shows OK"
+elif [ $rc -eq 2 ]
+then echo "postgres check on $HOST:$PORT shows WARNING"
+elif [ $rc -eq 3 ]
+then echo "postgres check on $HOST:$PORT shows CRITICAL"
+elif [ $rc -eq 4 ]
+then echo "postgres check on $HOST:$PORT shows UNKNOWN"
+fi
+done
